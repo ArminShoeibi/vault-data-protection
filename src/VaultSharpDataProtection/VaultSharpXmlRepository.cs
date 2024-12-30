704cb14585151ce85dff2a84933dcc7c9af4e5a5
ShoeibiNejad.Armin <ShoeibiNejad.Armin@Okala.com>
2024-12-31 01:58:10 +0330

    Implement IDeletableXmlRepository

--- src/VaultSharpDataProtection/VaultSharpXmlRepository.cs
+++ src/VaultSharpDataProtection/VaultSharpXmlRepository.cs
@@ -16,7 +16,7 @@ namespace VaultSharpDataProtection;
 /// data protection keys in Vault using the VaultSharp client.
 /// </summary>
 /// <seealso cref="Microsoft.AspNetCore.DataProtection.Repositories.IXmlRepository" />
-public class VaultSharpXmlRepository : IXmlRepository
+public class VaultSharpXmlRepository : IDeletableXmlRepository
 {
     private static readonly TaskFactory _factory =
         new TaskFactory(
@@ -48,11 +48,11 @@ public class VaultSharpXmlRepository : IXmlRepository
     {
         try
         {
-            var response = RunSync(() => _vault.V1.Secrets.KeyValue.V2.ReadSecretAsync(_path, null, _mountPoint));
+            var response = RunSync(async () => await _vault.V1.Secrets.KeyValue.V2.ReadSecretAsync(_path, null, _mountPoint));
             return response.Data.Data.Values.Select(e => e switch
             {
                 string v => XElement.Parse(v),
-                JsonElement { ValueKind: JsonValueKind.String } j when j.GetString() is {} s => XElement.Parse(s),
+                JsonElement { ValueKind: JsonValueKind.String } j when j.GetString() is { } s => XElement.Parse(s),
                 _ => throw new InvalidCastException($"Expected JSON string, but got {e.GetType()}.")
             }).ToList()
               .AsReadOnly();
@@ -102,5 +102,78 @@ public class VaultSharpXmlRepository : IXmlRepository
             .Unwrap()
             .GetAwaiter()
             .GetResult();
+    }
+
+    /// <inheritdoc/>
+    public bool DeleteElements(Action<IReadOnlyCollection<IDeletableElement>> chooseElements)
+    {
+        ArgumentNullException.ThrowIfNull(chooseElements);
+        try
+        {
+            return RunSync<bool>(async () =>
+            {
+                var response = await _vault.V1.Secrets.KeyValue.V2.ReadSecretAsync(_path, null, _mountPoint);
+                int? version = response.Data.Metadata.Version;
+                IDictionary<string, object> value = response.Data.Data;
+
+                if (value.Count < 1 || version is null)
+                {
+                    return false;
+                }
+
+                var deletableElements = value.Select(kv =>
+                {
+                    var element = kv.Value switch
+                    {
+                        string v => XElement.Parse(v),
+                        JsonElement { ValueKind: JsonValueKind.String } j when j.GetString() is { } s => XElement.Parse(s),
+                        _ => throw new InvalidCastException($"Expected JSON string, but got {kv.Value.GetType()}.")
+                    };
+                    var id = element.Attribute("id")?.Value;
+                    if (string.IsNullOrWhiteSpace(id) || id.Equals(kv.Key, StringComparison.OrdinalIgnoreCase) is false)
+                    {
+                        throw new InvalidOperationException("Element is missing the 'id' attribute.");
+                    }
+                    return new DeletableElement(kv.Key, element);
+                }).ToList();
+
+                chooseElements(deletableElements);
+
+                var elementsToDelete = deletableElements.Where(e => e.DeletionOrder.HasValue)
+                                                        .OrderBy(e => e.DeletionOrder.GetValueOrDefault())
+                                                        .ToList();
+
+                if (elementsToDelete is null || elementsToDelete.Count < 1)
+                {
+                    return false;
+                }
+
+                foreach (var element in elementsToDelete)
+                {
+                    value.Remove(element.Key);
+                }
+
+                await _vault.V1.Secrets.KeyValue.V2.WriteSecretAsync(_path, value, version, _mountPoint);
+                // TODO: check  the write is successful then delete the old(current) version
+                await _vault.V1.Secrets.KeyValue.V2.DeleteSecretVersionsAsync(_path, versions: [version.GetValueOrDefault()], _mountPoint);
+                return true;
+            });
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    private sealed class DeletableElement : IDeletableElement
+    {
+        public DeletableElement(string key, XElement element)
+        {
+            Key = key;
+            Element = element;
+        }
+        public XElement Element { get; }
+        public int? DeletionOrder { get; set; }
+        public string Key { get; }
     }
 }
